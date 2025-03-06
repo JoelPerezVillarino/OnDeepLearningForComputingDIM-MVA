f41478cabbb1ce7d7bfce8ce658c2bdfc3da8e61
JoelPerezVillarino <joel.perez.villarino@udc.es>
2025-03-06 16:17:40 +0100

    execute

--- execute_generation.py
+++ execute_generation.py
@@ -7,4 +7,4 @@ from src import DataGen
 json_path = os.path.join(os.getcwd(), "hull_white_data_portfolio.json")
 gen = DataGen.from_json(json_path)
 gen.gen_train_set()
-# gen.gen_val_set()
+gen.gen_val_set()
