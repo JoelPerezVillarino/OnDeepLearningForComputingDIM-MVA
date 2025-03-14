d88e95126488639eec2fe316b72c6c18cfdec852
JoelPerezVillarino <joel.perez.villarino@udc.es>
2025-03-14 19:05:43 +0100

    update codes

--- execute_generation.py
+++ execute_generation.py
@@ -2,14 +2,14 @@ import os
 from src import DataGen
 
 
-json_path = os.path.join(os.getcwd(), "json_files", "cir_data.json")
+# json_path = os.path.join(os.getcwd(), "json_files", "cir_data.json")
 # json_path = os.path.join(os.getcwd(), "cir_data_2.json")
 # json_path = os.path.join(os.getcwd(), "hull_white_data.json")
-# json_path = os.path.join(os.getcwd(), "json_files", "hull_white_data_portfolio.json")
+json_path = os.path.join(os.getcwd(), "json_files", "hull_white_data_portfolio.json")
 gen = DataGen.from_json(json_path)
-# gen.gen_train_set()
-# gen.gen_val_set()
-gen.val_set_variance()
+gen.gen_train_set()
+gen.gen_val_set()
+# gen.val_set_variance()
 
 # json_path = os.path.join(os.getcwd(), "json_files", "hull_white_extreme.json")
 # gen = DataGen.from_json(json_path)
